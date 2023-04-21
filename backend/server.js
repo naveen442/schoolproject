b21c361d049f927f92cb1b462ea8c18b1c1fd412
unknown <you@example.com>
2023-04-21 18:22:00 +0530

    db connected

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express=require('express');
 require('dotenv').config();
+const connectDB=require('./config/db');
 const port=process.env.PORT ||5000;
-const routes=require('./routes/goalRroutes')
+const routes=require('./routes/goalRroutes');
+const color=require('colors');
 const app=express();
+connectDB();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use('/api/goals',routes)
